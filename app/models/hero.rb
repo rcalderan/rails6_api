bcbee69d36afc56cc4a33351f67122db5d95bd56
Richard Calderan <richardcck@hotmail.com>
2023-05-12 15:36:28 -0300

    Add Namespace 'API' and order heroes

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,16 @@
 class Hero < ApplicationRecord
+
+  validates :name, presence: true
+
+
+
+  # Ex:- scope :active, -> {where(:active => true)}
+
+  scope :sorted_by_name, -> { order(:name) }
+  #another way to do the same:
+  #scope :sorted_by_name, -> { order(name => :desc) }
+  #order by desc:
+  # scope :sorted_by_name, -> { order(name: :desc) }
+
 end
+
